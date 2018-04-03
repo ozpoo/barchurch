02a8f5ab1e2dd1f0d876292b959ba5ec59b04930
ozpoo <hello@ericandren.com>
2018-04-03 16:10:54 -0700

    Stuff

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 
 				var init = function() {
 					$("iframe").each(function(){
-						$(this).attr("data-ratio", $(this).attr("height")/$(this).attr("width"));
+						$(this).attr("data-ratio", $(this).attr("width")/$(this).attr("height"));
 						$(this).removeAttr("height");
 						$(this).removeAttr("width");
 					});
